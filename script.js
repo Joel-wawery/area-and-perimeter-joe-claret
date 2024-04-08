edf0f0a513abc9df802502aa27b17a65a056c61d
Joel-wawery <163989746+Joel-wawery@users.noreply.github.com>
2024-04-08 12:17:34 +0300

    this is area and perimeter

---
+++ script.js
@@ -0,0 +1,13 @@
+// Define length and width
+let length = 5;
+let width = 10;
+
+// Calculate area
+let area = length * width;
+
+// Calculate perimeter
+let perimeter = 2 * (length + width);
+
+// Output results
+console.log("Area:", Area);
+console.log("Perimeter:", perimeter);
